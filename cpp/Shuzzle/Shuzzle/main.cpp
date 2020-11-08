deea7604272e1154cb5a6d5f04b8a1a95972dd40
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-11-07 16:42:25 -0800

    Staring C++ project with XCode

---
+++ cpp/Shuzzle/Shuzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Shuzzle
+//
+//  Created by Lewey Geselowitz on 11/7/20.
+//  Copyright © 2020 Lewey Geselowitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
