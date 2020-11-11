bc70e65bbcdd5c5a0ced1065e0cb10b17f90d4b3
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-11-10 22:48:54 -0800

    Basic board hover working

--- web/freed_go/freed_go.js
+++ web/freed_go/freed_go.js
@@ -3,17 +3,33 @@ var FreedGoGame_Create = function() {
     var ans = Object.create( FreedGoPrototype_Game );
     ans.Setup();
     return ans;
-}
+};
 
 var FreedGoPrototype_State = {
     Game : null,
     Core : {
         Turn : 0,
         Nodes : [],
+        Taken : [],
+        Hovers : [],
     },
 
     Setup : function(game) {
         this.Game = game;
+        this.NewBoard();
+    },
+
+    NewBoard : function() {
+        var board = this.Game.Board;
+        var numNodes = board.Nodes.length;
+        this.Core.Turn = 0;
+        this.Core.Nodes = []; 
+        this.Core.Nodes.length = numNodes;
+        this.Core.Taken = [ 0, 0 ];
+        this.Core.Hovers = [ -1, -1 ];
+        for (var i=0; i<numNodes; i++) {
+            this.Core.Nodes[i] = -1;
+        }
     },
 };
 
@@ -21,14 +37,45 @@ var FreedGoThreeJS_Prototype = {
     Game : null,
     SceneParent : null,
     Stones : [],
+    Cursors : [],
+    MatsByType : {},
 
     Setup : function(game) {
         this.Game = game;
     },
 
+    Update : function() {
+
+        for (var ni=0; ni<this.Stones.length; ni++) {
+            var state = this.Game.State.Core.Nodes[ni];
+            var from = this.Stones[ni];
+            var mat = this.MatsByType[ state ];
+            if (from.material != mat) {
+                from.material = mat;
+            }
+        }
+
+        for (var ti=0; ti<2; ti++) {
+            var cursor = this.Cursors[ti];
+            var hover = this.Game.State.Core.Hovers[ti];
+            if (hover >= 0) {
+                var over = this.Stones[ hover ];
+                cursor.position.copy( over.position );
+                cursor.visible = true;
+            } else {
+                cursor.visible = false;
+            }
+        }
+    },
+
     Build : function( scene ) {
         this.SceneParent = scene;
         this.Stones = [];
+
+        this.MatsByType = {};
+        this.MatsByType[-1] = new THREE.MeshLambertMaterial( { color: 0xccCCcc } );
+        this.MatsByType[ 0] = new THREE.MeshLambertMaterial( { color: 0xffFFff } );
+        this.MatsByType[ 1] = new THREE.MeshLambertMaterial( { color: 0x333333 } );
 
         const geometry = new THREE.SphereBufferGeometry( 3.141, 12, 12 );
         const board_scale = 35.0;
@@ -40,10 +87,10 @@ var FreedGoThreeJS_Prototype = {
         for (var ni=0; ni<nodeInfo.length; ni++) {
             var node = nodeInfo[ni];
 
-            const stone = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+            const stone = new THREE.Mesh( geometry, this.MatsByType[-1] );
             stone.position.copy( node.Position );
             stone.position.multiplyScalar( board_scale );
-            stone.userData = node;
+            stone.userData.Node = node;
             this.Stones.push( stone );
 
             lookTo.copy( node.Normal );
@@ -59,23 +106,36 @@ var FreedGoThreeJS_Prototype = {
             this.SceneParent.add( stone );
         }
 
-        var linePoints = [];
-        for (var ni=0; ni<nodeInfo.length; ni++) {
-            var node = nodeInfo[ni];
-            var from = this.Stones[ni];
-            for (var ei=0; ei<node.Neighbors.length; ei++) {
-                var to = this.Stones[ node.Neighbors[ei] ];
-                linePoints.push( from.position.clone() );
-                linePoints.push( to.position.clone() );
+        if (true) {
+            var linePoints = [];
+            for (var ni=0; ni<nodeInfo.length; ni++) {
+                var node = nodeInfo[ni];
+                var from = this.Stones[ni];
+                for (var ei=0; ei<node.Neighbors.length; ei++) {
+                    var to = this.Stones[ node.Neighbors[ei] ];
+                    linePoints.push( from.position.clone() );
+                    linePoints.push( to.position.clone() );
+                }
+            }
+            const lineMat = new THREE.LineBasicMaterial({
+                color: 0x000000
+            });
+            const lineGeo = new THREE.BufferGeometry().setFromPoints( linePoints );
+            const lineObj = new THREE.LineSegments( lineGeo, lineMat );
+            this.SceneParent.add( lineObj );
+        }
+
+        if (true) {
+            this.Cursors = [ null, null ];
+            for (var ti=0; ti<2; ti++) {
+                const cursorObj = new THREE.Mesh( geometry, this.MatsByType[ti] );
+                cursorObj.scale.set( 0.5, 0.5, 0.5 );
+                this.SceneParent.add( cursorObj );
+                this.Cursors[ti] = cursorObj;
             }
         }
-        const lineMat = new THREE.LineBasicMaterial({
-            color: 0x000000
-        });
-        const lineGeo = new THREE.BufferGeometry().setFromPoints( linePoints );
-        const lineObj = new THREE.LineSegments( lineGeo, lineMat );
-        this.SceneParent.add( lineObj );
 
+        this.Update();
     }
 };
 
@@ -90,5 +150,13 @@ var FreedGoPrototype_Game = {
         this.State.Setup( this );
         this.Render = Object.create( FreedGoThreeJS_Prototype );
         this.Render.Setup( this );
+    },
+
+    OnHoveredOver : function(index) {
+        this.State.Core.Hovers[ this.State.Core.Turn ] = index;
+    },
+
+    Update : function() {
+        this.Render.Update();
     },
 };
