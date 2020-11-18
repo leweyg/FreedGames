c1377380b97d8e71fb1b229e3e5b944d24c8c798
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-11-18 01:10:51 -0800

    Rendering of blocks into voxels

--- docs/Shuzzle/shuzzle_core.js
+++ docs/Shuzzle/shuzzle_core.js
@@ -8,6 +8,11 @@ var ShuzzleSaveCallbacks = {
     }),
 };
 
+var numOr0 = function(v) {
+    if (v) return v;
+    return 0;
+}
+
 var TensorMath = {
     cloneVector3 : function(v) {
         return { x:v.x, y:v.y, z:v.z };
@@ -24,6 +29,12 @@ var TensorMath = {
             max:TensorMath.cloneVector3(b.max),
         };
     },
+    isVector3InBounds : function(v,b) {
+        var x = ((v.x >= b.min.x)&&(v.x <= b.max.x));
+        var y = ((v.y >= b.min.y)&&(v.y <= b.max.y));
+        var z = ((v.z >= b.min.z)&&(v.z <= b.max.z));
+        return (x && y && z);
+    },
     clampVector3InBounds : function(v,b) {
         v.x = Math.max( b.min.x, Math.min( b.max.x, v.x ) );
         v.y = Math.max( b.min.y, Math.min( b.max.y, v.y ) );
@@ -31,6 +42,11 @@ var TensorMath = {
     },
     vector3MaxAxisDistanceXY : function(a,b) {
         return Math.min( Math.abs(a.x-b.x), Math.abs(a.y-b.y));
+    },
+    vector3ApplyTransform : function(v,t,into) {
+        into.x = (numOr0(t.x.x) * v.x) + (numOr0(t.x.y) * v.y) + (numOr0(t.x.z) * v.z);
+        into.y = (numOr0(t.y.x) * v.x) + (numOr0(t.y.y) * v.y) + (numOr0(t.y.z) * v.z);
+        into.z = (numOr0(t.z.x) * v.x) + (numOr0(t.z.y) * v.y) + (numOr0(t.z.z) * v.z);
     },
     _tempVector3 : new THREE.Vector3(),
     boundsMaxAxisDistanceXY : function(b,v) {
@@ -53,6 +69,70 @@ var TensorMath = {
     },
 };
 
+var ShuzzlePrototype_Voxels = {
+    Size : new THREE.Vector3(),
+    Bounds : null,
+    VoxelData : [],
+    TotalVoxels : 0,
+    _TempForIndexing : new THREE.Vector3(),
+
+    SetupFromBounds : function(_bounds) {
+        this.Bounds = _bounds;
+        this.Size.copy( this.Bounds.max );
+        this.Size.sub( this.Bounds.min );
+
+        this.TotalVoxels = this.Size.x * this.Size.y * this.Size.z;
+        this.VoxelData = []; this.VoxelData.length = this.TotalVoxels;
+        for (var vi=0; vi<this.TotalVoxels; vi++) {
+            this.VoxelData[vi] = ' ';
+        }
+    },
+
+    SafeGetIndex : function(v) {
+        if (TensorMath.isVector3InBounds(v, this.Bounds)) {
+            var i = ( ((v.x - this.Bounds.min.x)*1)
+                + ((v.y - this.Bounds.min.y)*this.Size.x)
+                + ((v.z - this.Bounds.min.z)*(this.Size.x * this.Size.y)) );
+            if ((i <= 0) && (i >= this.TotalVoxels)) {
+                alert("Indexing error: " + i + " for " + v);
+            }
+            return i;
+        }
+        return -1;
+    },
+    
+    TryGetBoxVoxelIndex : function (blockState, boxLocalPos ) {
+        var t= this._TempForIndexing;
+        TensorMath.vector3ApplyTransform( boxLocalPos, blockState.RotationTrans, t );
+        t.add( blockState.Center );
+        var ndx = this.SafeGetIndex( t );
+        return ndx;
+    },
+
+    DrawBlock : function(blockState, blockDef, letter) {
+        var nBoxes = blockDef.Boxes.length;
+        if (letter === undefined) letter = blockDef.Letter;
+        for (var bi=0; bi<nBoxes; bi++) {
+            var boxLocalPos = blockDef.Boxes[bi];
+            var ndx = this.TryGetBoxVoxelIndex( blockState, boxLocalPos );
+            if (ndx >= 0) {
+                var was = this.VoxelData[ndx];
+                if ((was==" ")||(letter==" ")) {
+                    this.VoxelData[ndx] = letter;
+                } else {
+                    throw "Invalid voxel write over '" + was + "'";
+                }
+            }
+        }
+    },
+
+    ClearAll : function() {
+        for (var vi=0; vi<this.TotalVoxels; vi++) {
+            this.VoxelData[vi] = ' ';
+        }
+    },
+};
+
 var ShuzzlePrototype_State = {
     Game : null,
     Fast : {
@@ -63,6 +143,8 @@ var ShuzzlePrototype_State = {
         BoardName : "1",
         Blocks : [],
     },
+    Voxels : ShuzzlePrototype_Voxels,
+
     OnCoreUpdated : [],
     OnFastUpdated : [],
 
@@ -85,14 +167,21 @@ var ShuzzlePrototype_State = {
         var bounds = this.Game.Board.Board.Bounds;
         this.Fast.LightPos = TensorMath.cloneVector3( bounds.max );
         this.Fast.LightPos.x = ( bounds.min.x + bounds.max.x ) * 0.5;
+
+        this.Voxels = Object.create( ShuzzlePrototype_Voxels );
+        this.Voxels.SetupFromBounds( bounds );
+
         //this.Fast.LightPos.x = this.Game.Board.Board.Bounds.min.x;
         for (var i=0; i<numNodes; i++) {
+            board.Blocks[i].Letter = String.fromCharCode( board.Blocks[i].Index + "a".charCodeAt(0) );
             this.Core.Blocks[i] = { 
                 Center : {x:0, y:0, z:0},
-                Rotation : {
+                RotationTrans : {
                     x:{x:1}, y:{y:1}, z:{z:1}
                 }
             };
+            TensorMath.copyVector3Into( this.Core.Blocks[i].Center, board.Blocks[i].Center );
+            this.Voxels.DrawBlock( this.Core.Blocks[i], board.Blocks[i] );
         }
     },
 
@@ -110,7 +199,7 @@ var ShuzzlePrototype_State = {
 
     DoCoreChangedRemote : function() {
         if (this.Core.BoardName != this.Game.Board.Name) {
-            this.Game.Board 
+            //this.Game.Board 
         }
         for (var i in this.OnCoreUpdated) {
             this.OnCoreUpdated[i]();
